43dd1e92683322cb719e421a95533f1c656c1680
Ryo Fujiwara <jjcproject@icloud.com>
2020-02-24 22:52:31 +0900

    got the function, not try to make it so an unordered listing can also work

--- until.rb
+++ until.rb
@@ -11,20 +11,31 @@ sax_part = gets.chomp.downcase
 # this is the part that is confusing me.  Trying to figure out the method in which
 # a string in the above array matches an input, whether "alto saxophone 1" or "Eb alto saxophone 1"
 # is typed in. ("alto 1" is found in both)
-p saxophone_section.any?(sax_part)
+
 
 # I am thinking that this bottom parts one could be used? or not?
-parts = saxophone_section.map {|sax| sax.gsub(/\s+/m, ' ').strip.split(" ")}
+# parts = saxophone_section.map {|sax| sax.gsub(/\s+/m, ' ').strip.split(" ")}
 
 
-if saxophone_section.include?(sax_part) == true
+# if saxophone_section.include?(sax_part) == true
 
-p saxophone_section.delete_if{ |s| s == sax_part}
+# p saxophone_section.delete_if{ |s| s == sax_part}
+# puts "Damn, you're lucky"
+# else
+#   puts "Woops! Try again."
+# end
+# end
+# puts "You got all parts."
+
+
+
+
+if sax_part.split & saxophone_section[0].split == saxophone_section[0].split
+
+p saxophone_section.delete_if{ |s| s == saxophone_section[0]}
 puts "Damn, you're lucky"
 else
   puts "Woops! Try again."
 end
 end
 puts "You got all parts."
-
-
