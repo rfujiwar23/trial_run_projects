c9fb42e16571f16743a0b92a6bfc1676afc07b7c
Ryo Fujiwara <jjcproject@icloud.com>
2020-03-01 16:05:56 +0900

    look into test.rb for creating a morse code decoder and encoder

--- test.rb
+++ test.rb
@@ -1,9 +1,54 @@
+MORSE = {
+  "A" => ".-",
+  "B" => "-...",
+  "C" => "-.-.",
+  "D" => "-..",
+  "E" => ".",
+  "F" => "..-.",
+  "G" => "--.",
+  "H" => "....",
+  "I" => "..",
+  "J" => ".---",
+  "K" => "-.-",
+  "L" => ".-..",
+  "M" => "--",
+  "N" => "-.",
+  "O" => "---",
+  "P" => ".--.",
+  "Q" => "--.-",
+  "R" => ".-.",
+  "S" => "...",
+  "T" => "-",
+  "U" => "..-",
+  "V" => "...-",
+  "W" => ".--",
+  "X" => "-..-",
+  "Y" => "-.--",
+  "Z" => "--.."
+}
 
-  s = "run this"
-  num = s.length
-  if num > 1 && num < 20 && s.match(/\w/)
-    puts "Hello #{s}"
-  end
+def encode(text)
+  words = text.gsub(/[,.!?']/, '').split(' ')
+  words.map { |word| encode_word(word) }.join('|')
+end
+
+def encode_word(word)
+  word.split('').map { |letter| MORSE[letter.upcase] }.join(' ')
+end
 
 
+def decode(morse_text)
+  morse_text.split(" ").map { |morse_word| decode_word(morse_word) }.join("")
+end
+
+def decode_word(morse_word)
+  morse_word.split(" ").map { |morse_letter| MORSE.key(morse_letter) }.join
+end
+
+
+
+
+
+puts encode_word("Hello World")
+puts decode(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
 
--- track.rb
+++ track.rb
@@ -1,33 +1,91 @@
-ROMAN_TO_INT = {
-  "I" => 1,
-  "IV" => 4,
-  "V" => 5,
-  "IX" => 9,
-  "X" => 10,
-  "XL" => 40,
-  "L" => 50,
-  "XC" => 90,
-  "C" => 100,
-  "CD" => 400,
-  "D" => 500,
-  "CM" => 900,
-  "M" => 1000
-}
-puts "put roman number:"
-print ">>"
-roman_string = gets.chomp
+# ROMAN_TO_INT = {
+#   "I" => 1,
+#   "IV" => 4,
+#   "V" => 5,
+#   "IX" => 9,
+#   "X" => 10,
+#   "XL" => 40,
+#   "L" => 50,
+#   "XC" => 90,
+#   "C" => 100,
+#   "CD" => 400,
+#   "D" => 500,
+#   "CM" => 900,
+#   "M" => 1000
+# }
+# puts "put roman number:"
+# print ">>"
+# roman_string = gets.chomp
 
 
-  number = 0
-  str = roman_string.dup
-  until str.size.zero?
-    last_two_characters = str.slice(-2, 2)
-    if ROMAN_TO_INT.key?(last_two_characters)
-      number += ROMAN_TO_INT[last_two_characters]
-      str.chop!
-    else
-      number += ROMAN_TO_INT[str.slice(-1)]
+#   number = 0
+#   str = roman_string.dup
+#   until str.size.zero?
+#     last_two_characters = str.slice(-2, 2)
+#     if ROMAN_TO_INT.key?(last_two_characters)
+#       number += ROMAN_TO_INT[last_two_characters]
+#       str.chop!
+#     else
+#       number += ROMAN_TO_INT[str.slice(-1)]
+#     end
+#     str.chop!
+#   end
+#   puts number
+
+
+def in_words(int)
+  numbers_in_letters = {
+      1000000 => "million",
+      1000 => "thousand",
+      100 => "hundred",
+      90 => "ninety",
+      80 => "eighty",
+      70 => "seventy",
+      60 => "sixty",
+      50 => "fifty",
+      40 => "forty",
+      30 => "thirty",
+      20 => "twenty",
+      19=>"nineteen",
+      18=>"eighteen",
+      17=>"seventeen",
+      16=>"sixteen",
+      15=>"fifteen",
+      14=>"fourteen",
+      13=>"thirteen",
+      12=>"twelve",
+      11 => "eleven",
+      10 => "ten",
+      9 => "nine",
+      8 => "eight",
+      7 => "seven",
+      6 => "six",
+      5 => "five",
+      4 => "four",
+      3 => "three",
+      2 => "two",
+      1 => "one",
+      0 => "zero"
+    }
+  string = ""
+  numbers_in_letters.each do |num, name|
+    if int == 0
+      return string
+    elsif int.to_s.length == 1 && int/num > 0
+      return string + "#{name}"
+    elsif int < 100 && int/num > 0
+      return string + "#{name}" if int%num == 0
+      return string + "#{name} " + in_words(int%num)
+    elsif int/num > 0
+      return string + in_words(int/num) + " #{name} " + in_words(int%num)
     end
-    str.chop!
   end
-  puts number
+end
+
+
+
+puts in_words(4)
+puts in_words(27)
+puts in_words(102)
+puts in_words(380079)
+# puts in_words(82102713.0)
